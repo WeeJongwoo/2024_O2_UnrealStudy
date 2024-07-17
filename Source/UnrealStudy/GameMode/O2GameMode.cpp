791313d21f5a6101e7c3da90c2b2008c0d31209e
PirA <wjw0518@gmail.com>
2024-07-18 02:03:03 +0900

    Week3_FinalRevise

--- Source/UnrealStudy/GameMode/O2GameMode.cpp
+++ Source/UnrealStudy/GameMode/O2GameMode.cpp
@@ -6,13 +6,13 @@
 AO2GameMode::AO2GameMode()
 {
 
-	ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Game/Blueprint/Character/BP_PlayerCharacter.BP_PlayerCharacter_C"));
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Game/Blueprint/Character/BP_PlayerCharacter.BP_PlayerCharacter_C"));
 	if (DefaultPawnClassRef.Class)
 	{
 		DefaultPawnClass = DefaultPawnClassRef.Class;
 	}
 
-	ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/UnrealStudy.O2PlayerController"));
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/UnrealStudy.O2PlayerController"));
 	if (PlayerControllerClassRef.Class)
 	{
 		PlayerControllerClass = PlayerControllerClassRef.Class;
